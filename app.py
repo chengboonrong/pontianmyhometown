6ec45e7b26da08d5d21577f6dda4be5a88cc4f99
chengboonrong <chengchris8715@gmail.com>
2020-07-20 11:49:37 +0800

    CSS updated 1.1

--- app.py
+++ app.py
@@ -15,26 +15,26 @@ rain_clf = pickle.load(open('./models/my_rain_model.sav', 'rb'))
 app = Flask(__name__)
 app.static_folder = 'static'
 
-keys = ['apparentTemperature', 'humidity', 'windSpeed', 'visibility', 'pressure']
-
 ### openweatherAPI
 API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=Pontian,my&appid=e357f3a75f4a8d5c96ccb0742cccdc27&units=metric'
 # API_KEY = 'e357f3a75f4a8d5c96ccb0742cccdc27'
 # raw_data = requests.get(API_URL).json()
 # print(raw_data)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def home():
     raw_data = requests.get(API_URL).json()
     # print(raw_data)
-    weather_data = [raw_data['main']['temp'], raw_data['main']['humidity'], raw_data['wind']['speed'], raw_data['visibility'], raw_data['main']['pressure'], raw_data['clouds']['all']]
+    weather_data = [round(raw_data['main']['temp'], 0), raw_data['main']['humidity'], raw_data['wind']['speed'], raw_data['visibility'], raw_data['main']['pressure'], raw_data['clouds']['all']]
     # print(weather_data)
 
-    cor = rain_clf.predict(np.array(weather_data).reshape(1, -1))[0]
-    cof = round(flood_clf.predict(np.array(weather_data).reshape(1, -1))[0], 1)
-    prediction = [cor , cof]
+    cor = round(rain_clf.predict(np.array(weather_data).reshape(1, -1))[0], 1)
 
-    return render_template('home.html', data=raw_data, cor=cor, cof=cof)
+    return render_template('home.html', raw=raw_data ,data=weather_data, cor=cor)
+
+@app.route('/test')
+def test():
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
